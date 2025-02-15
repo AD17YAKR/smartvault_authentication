05d34b6e390a03e002ae09f974709c5222a733aa
AD17YAKR <adityasingh.110601@gmail.com>
2025-02-16 01:20:24 +0530

    Initial Commit

---
+++ src/main/java/com/smartvault/authentication/SmartvaultAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.smartvault.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartvaultAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartvaultAuthenticationApplication.class, args);
+    }
+
+}
